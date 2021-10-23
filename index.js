568d948d8bb96d50eae686936ce5341b9350b664
Mario <opnfuso@gmail.com>
2021-10-23 18:14:41 -0500

    Redirect

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ require("dotenv").config();
 
 const app = express();
 
-const PORT = process.env.PORT;
+const connection = require("./db.config");
+connection.once("open", () => console.log("DB Connected"));
+connection.on("error", () => console.log("Error"));
 
+app.use(
+  express.json({
+    extended: false,
+  })
+);
+
+app.use("/", require("./redirect"));
+app.use("/api/url", require("./url"));
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log("Server started on port " + PORT));
